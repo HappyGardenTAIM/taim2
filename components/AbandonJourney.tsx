51c49d2679e15394e9858263012ef39d2008bd23
Helena Šegedin <helena.segedin@vikk.ee>
2024-05-08 12:12:42 +0300

    As a user I want to be able to abandon a journey before completion [Delivers #187562251]

---
+++ components/AbandonJourney.tsx
@@ -0,0 +1,105 @@
+import { gql, useMutation } from '@apollo/client';
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import AbandonJourneyConfirmation from './AbandonJourneyConfirm';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+const ABANDON_JOURNEY = gql`
+  mutation AbandonJourney($journeyId: Int!, $status: JourneyStatus!) {
+    updateJourneyStatus(data: {journeyId: $journeyId, status: $status}) {
+      id
+    }
+ }
+`;
+
+interface AbandonJourneyProps {
+  journeyId: number;
+}
+
+const AbandonJourney: React.FC<AbandonJourneyProps> = ({ journeyId }) => {
+  const [confirmDelete, setConfirmDelete] = React.useState(false);
+  const [abandonJourney] = useMutation(ABANDON_JOURNEY);
+
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+  
+  const handleConfirmDelete = () => {
+    // Perform the deletion
+    abandonJourney({
+      variables: { journeyId, status: 'ABANDONED' },
+    });
+    // Reset confirmation state
+    toggleModal();
+    // Navigate back to the home screen
+    navigation.navigate('UserHomeScreen');
+  }
+  
+  const toggleModal = () => {
+    // Reset confirmation state
+    setConfirmDelete(!confirmDelete);
+  }
+
+  return (
+    <View>
+      <TouchableOpacity style={styles.deleteButton} onPress={toggleModal}>
+        <Text style={styles.buttonLabel}>Kustuta taim</Text>
+      </TouchableOpacity>
+
+      {confirmDelete && (
+        <AbandonJourneyConfirmation
+          onConfirm={handleConfirmDelete}
+          onCancel={toggleModal}
+          visible={confirmDelete}
+        />
+      )}
+    </View>
+  );
+}
+
+export default AbandonJourney;
+
+// delete button styles
+
+const styles = StyleSheet.create({
+  deleteButton: {
+    backgroundColor: '#c70000',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '85%',
+    alignItems: 'center',
+    alignSelf: 'center',    
+  },
+  buttonLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center', // Center the text horizontally
+    maxWidth: '100%', // Ensure text doesn't overflow button width
+  },
+  confirmationContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '85%',
+    alignItems: 'center',
+    alignSelf: 'center',
+    elevation: 5,
+  },
+  confirmationText: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  confirmButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+});
---
+++ components/AbandonJourneyConfirm.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+interface AbandonJourneyConfirmationProps {
+  onConfirm: () => void;
+  onCancel: () => void;
+  visible: boolean;
+}
+
+const AbandonJourneyConfirmation: React.FC<AbandonJourneyConfirmationProps> = ({ onConfirm, onCancel, visible }) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onCancel}
+    >
+      <TouchableOpacity
+        style={styles.popupContainer}
+        activeOpacity={1}
+        >     
+        <View style={styles.popUpTextBackground}>
+          <TouchableOpacity style={styles.closeButton} onPress={onCancel}>
+            <Icon name="times" size={20} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.confirmationText}>Kindel? Kõik taime andmed kustuvad!</Text>
+          <TouchableOpacity style={styles.confirmButton} onPress={onConfirm}>
+              <Text style={styles.buttonLabel}>Jah, kustuta</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.cancelButton} onPress={onCancel}>
+              <Text style={styles.buttonLabel}>Ära kustuta</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+}
+
+export default AbandonJourneyConfirmation;
+
+// delete button styles
+
+const styles = StyleSheet.create({
+  popUpTextBackground: {
+    backgroundColor: '#93C392',
+    borderRadius: 30,
+    padding: 20,
+    width: '80%',
+  },
+  confirmationText: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 15,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  confirmButton: {
+    backgroundColor: '#c70000',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '85%',
+    alignSelf: 'center',
+  },
+  cancelButton: {
+    backgroundColor: '#caffa8',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '85%',
+    alignSelf: 'center',
+  },
+  buttonLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  popupContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,    
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+},
+});
